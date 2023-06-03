27c734e5642907b2169321807410ffbe03602e12
Paco <elpaquinho@gmail.com>
2023-06-03 18:11:40 +0200

    funcion paginador tablas

--- modelos/mainModel.php
+++ modelos/mainModel.php
@@ -80,4 +80,61 @@
 
             return $cadena;
         }
+
+        // función verificar datos
+        protected static function verificar_datos($filtro, $cadena) {
+            if(preg_match("/^" . $filtro . "$/", $cadena)) {
+                return false;
+            }else {
+                return true;
+            }
+        }
+
+        // función verificar fechas
+        protected static function verificar_fecha($fecha) {
+            $valores = explode('-', $fecha);
+            if(count($valores) == 3 && checkdate($valores[1], $valores[2], $valores[0])) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+
+        // funcion paginador tablas
+        protected static function paginador_tablas($pagina, $Npaginas, $url, $botones) {
+            $tabla = '<nav aria-label="Page navigation example"><ul class="pagination justify-content-center">';
+
+            if($pagina == 1) {
+                $tabla .= '<li class="page-item disabled"><a class="page-link"><i class="fas fa -angel-double-left"></i></a></li>';    
+            } else {
+                $tabla .= '
+                <li class="page-item disabled"><a class="page-link" href="' . $url . '1/"><i class="fas fa -angel-double-left"></i></a></li>
+                <li class="page-item disabled"><a class="page-link" href="' . $url . ($pagina-1) . '/">Anterior</a></li>';
+            }
+
+            $ci = 0;
+            for($i = $pagina; $i <= $Npaginas; $i++) {
+                if($ci >= $botones) {
+                    break; 
+                }
+                
+                if($pagina == $i) {
+                    $tabla .= '<li class="page-item"><a class="page-link active" href="' . $url . $i . '/">' . $i .' </a></li>';    
+                } else {
+                    $tabla .= '<li class="page-item"><a class="page-link href="' . $url . $i . '/">' . $i .' </a></li>';
+                }
+                $ci++;
+            }
+
+            if($pagina == $Npaginas) {
+                $tabla .= '<li class="page-item disabled"><a class="page-link"><i class="fas fa -angel-double-right"></i></a></li>';    
+            } else {
+                $tabla .= '
+                <li class="page-item disabled"><a class="page-link" href="' . $url . ($pagina+1) . '/">Siguiente</a></li>
+                <li class="page-item disabled"><a class="page-link" href="' . $url . $Npaginas . '/"><i class="fas fa -angel-double-left"></i></a></li>';   
+            }
+
+            $tabla .= '</ul></nav>';
+            return $tabla;
+        }
     }
